e778ff2e5b957875965e4fcc05481aceb4c21fdd
juan-castrillon <castrillon.95@gmail.com>
2020-08-03 17:41:19 +0200

    Cambios en java

--- src/test/java/hello/Tester.java
+++ src/test/java/hello/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
     @Test
     public void GreeterTest() {
         Main greeter = new Main();
-        Assert.assertEquals("Hello World!", greeter.sayHello());
+        Assert.assertEquals("Hello World", greeter.sayHello());
     }
 
 }
