529f036c1305f657feee2fc917cbe327a9038041
juan-castrillon <castrillon.95@gmail.com>
2020-08-03 17:05:58 +0200

    subiendo todo

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Main {
+    public String sayHello() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) {
+        Main greeter = new Main();
+        System.out.println(greeter.sayHello());
+    }
+}
---
+++ src/test/java/hello/Tester.java
@@ -0,0 +1,12 @@
+package hello;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tester {
+    @Test
+    public void GreeterTest() {
+        Main greeter = new Main();
+        Assert.assertEquals("Hello World!", greeter.sayHello());
+    }
+
+}
