319798bd7af74b239b6c13ae84e48f88001676fb
juan-castrillon <castrillon.juan95@gmail.com>
2021-02-02 12:20:13 +0100

    Added docs in code, fixed gradle

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -1,9 +1,16 @@
 package hello;
 
 public class Main {
+
+    /**
+     * This function says hello world using a {@link String}
+     * @return Hello World!
+     */
     public String sayHello() {
         return "Hello World!";
     }
+
+
     public static void main(String[] args) {
         Main greeter = new Main();
         System.out.println(greeter.sayHello());
